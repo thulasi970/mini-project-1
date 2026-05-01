bc3eea13ea2253188d40bd7a5c5f87305703e183
Thulasika Ramachandran <UT011602@unicomtic.com>
2026-05-01 16:15:03 +0530

    create index,style and script

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('taskInput');
+const addtaskbutton = document.getElementById('addtaskbutton');
+const tasklist = document.getElementById('tasklist');
+const filterButtons = document.getElementById('.filter-button');
+let tasks = JSON.parse
